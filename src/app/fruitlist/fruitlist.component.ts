f19702a7c5f84995f0ba75a64e3aa408ef8a0cc3
Preston Jones <preston.jones@gmx.de>
2024-05-31 10:30:48 +0200

    save

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
